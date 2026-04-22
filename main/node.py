2e5363fa5df4c33fecbc573e71ba435e7813d40f
Akshata Vyas <akshatavyas01@gmail.com>
2026-04-22 18:26:02 +0530

    Initailized and created Summarization agent. Improved prompt format of the research agent.

--- main/node.py
+++ main/node.py
@@ -8,6 +8,7 @@ from langchain.tools import tool
 from langchain.agents import create_agent
 from langchain.agents.middleware import SummarizationMiddleware, ToolCallLimitMiddleware
 from langchain.messages import SystemMessage, HumanMessage
+from langchain_classic.prompts import PromptTemplate
 # from langchain_core.vectorstores import FAISS
 
 from langchain_groq import ChatGroq
@@ -88,25 +89,24 @@ wiki_tool_limit=ToolCallLimitMiddleware(tool_name='wikipedia_retriever_tool',thr
 websearch_tool_limit=ToolCallLimitMiddleware(tool_name='websearch_tool',thread_limit=5, run_limit=3)
 # RESEARCH AGENT
 
-def research_agent(state:graphState):
+def research_agent(state:graphState)-> graphState:
     topic=state.get('topic')
 
     prompt=SystemMessage(
-    content='''Think in step by step what all information do you need regrading a praticular topic to generate a summary and research report.
-    Tools that you can use: wikipedia_retriever_tool and websearch_tool.
-    Both the the tools accepts string query.
-    Keep filtering the data or information you recieve if they are not relevent.
-    But the information block you retrieve will act as base data for a research paper so it should be detailed.
-    It should contain all the sections of a research paper data such as:
-    1. abstract
-    2. introduction
-    3. methodology
-    4. results 
-    5. discussion
-    6. conclusion
-    7. references
-    
-    Everything from the above that is relevant to the topic. 
+    content='''Think in step by step method about the information required regrading a praticular topic to generate a summary and research report on it.
+    Instructions:-
+    1. Tools that you can use: wikipedia_retriever_tool and websearch_tool.(Both the the tools accepts string query)
+    2. Keep filtering the data or information you recieve if they are not relevent.
+`   3. Information block you retrieve will act as base data for a research paper so it should be detailed.
+        It should contain all the sections of a research paper data such as:
+        1. abstract
+        2. introduction
+        3. methodology
+        4. results 
+        5. discussion
+        6. conclusion
+        7. references  
+        Everything from the above that is relevant to the topic. 
     Note:- 
     1. RETURN A VERY BREIF AND DETAILED INFORMATION ON THE TOPIC.
     2. THE RETURNED INFORMATION SHOULD BETWEEN 5000 TO 10000 WORDS.(WORDS NOT TOKENs)
@@ -127,17 +127,51 @@ def research_agent(state:graphState):
     )
     if topic:
         results=agent.invoke({'messages':[HumanMessage(content=topic)]})
-        return {'reasearched_info': results['messages'][-1].content}
-
+        return {'researched_info': results['messages'][-1].content}
+    else:
+        return {'researched_info':'DATA ERROR'}
     
+#SUMMARIZATION AGENT
+    
+def summarization_agent(state:graphState)->graphState:
+    topic= state.get('topic')
+    researched_info=state.get('researched_info')
+    template='''
+    Act as an experet summarizer.
+    Instructions:
+    1. Generate a consise summary based on the Topic and Researched information.
+    2. The summary should be between 5-12 lines.
+    3. The summary should contain all important facts and keywords related to the topic.
+    4. Provide important insights.
 
-#Practice query:
+    Topic:{topic}
+    Researched information:{researched_info}
+    '''
+    if topic and researched_info:
+        prompt=PromptTemplate(template=template, input_variables=['topic','researched_info'])
+        final_prompt=prompt.format_prompt(topic=topic,researched_info=researched_info)
+        result=summery_model.invoke(final_prompt)
+        return {'summary':str(result.content)}
+    else:
+        return {'summary':'DATA ERROR'}
 
+
+
+
+
+# Practice query:
 # state:graphState={
-#     "topic":"WAR OF INDEPENDENCE: INDIA"
+#     "topic":"WAR OF INDEPENDENCE: INDIA",
+#     'researched_info':' ',
+#     'summary':' ',
+#     'report':{' ':'  '},
+#     'result':' '
 # }
 # results=research_agent(state)
-# print(results)
+# state.update(results)
+# result2=summarization_agent(state)
+# state.update(result2)
+# print(state)
 
 
 
