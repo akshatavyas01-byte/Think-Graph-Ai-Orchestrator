dd0f18241c0485047dab39d12399dabad426c8f3
Akshata Vyas <akshatavyas01@gmail.com>
2026-05-12 19:40:08 +0530

    Updated ui.py
    With resubmit options and display functions for results
    
    Added requirements.txt for the ui_service

--- main/ui_service/ui.py
+++ main/ui_service/ui.py
@@ -25,7 +25,7 @@ task=st.radio(
     )
 
 
-def request(topic:str, task:str):
+def request(topic:str, task:str, resubmit=False):
     try:
         with st.spinner(text="Loading",show_time=False):
             if task=="Summary":
@@ -40,15 +40,20 @@ def request(topic:str, task:str):
             else:
                 placeholder.write("SELECTION ERROR")
                 return 
-
-            response=httpx.get(
-                    url=url,
-                    params={"topic":topic},
-                    timeout=600.0
-                )
+            if task=="Information":
+                response=httpx.get(
+                        url=url,
+                        params={"topic":topic},
+                        timeout=600.0
+                    )
+            else :
+                response=httpx.get(
+                        url=url,
+                        params={"topic":topic, "resubmit":resubmit},
+                        timeout=600.0
+                    )
             response.raise_for_status()
             data = response.json()
-            print(data)
             st.session_state.result=data
             
     except Exception as e:
@@ -58,21 +63,55 @@ def request(topic:str, task:str):
 if st.button("Submit"):
     request(topic,task)
 
+def display_func(title, content):
+    if isinstance(content,list):
+        text="\n\n".join(f'- {item}' for item in content)
+    else:
+        text=str(content)
+    placeholder.markdown(f"#### {title}\n{text}")
+
+def display_report(report_content):
+    text=""
+    counter=1
+    if isinstance(report_content, list):
+        for values in (report_content):
+            summary=values.get("Summary")
+            report=values.get("Report")
+            feedback=values.get("Feedback")
+            text+=f"\n #### Report {counter}\n\n ##### Summary \n\n {summary}\n\n #### Report \n\n {report}\n\n ##### Feedback \n\n {feedback}\n\n"
+            counter+=1
+        placeholder.markdown(text)
+    else:
+        summary=report_content.get("Summary")
+        report=report_content.get("Report")
+        feedback=report_content.get("Feedback")
+        text+=f"\n #### Report \n\n ##### Summary \n\n {summary}\n\n #### Report \n\n {report}\n\n ##### Feedback \n\n {feedback}\n\n"
+        placeholder.markdown(text)
+
 
 if st.session_state.result:
     data=st.session_state.get("result")
-    print(data)
-    if all(k in data for k in ["Summary","Report","Feedback"]):
-        placeholder.markdown(f"#### Summary: \n{data.get('Summary')}\n #### Report:\n{data.get('Report')}\n #### Feedback: \n{data.get('Feedback')}")
-    elif "Summary" in data:
-        print(data.get('Summary'))
-        placeholder.markdown(f"#### Summary: \n {data.get('Summary')}")
-    elif "Information" in data:
-        placeholder.markdown(f"#### Information:\n {data.get('Information')}")
-   
-    
+    print(type(data))
+    if isinstance(data,dict):
+        print(data)
+        if all(k in data for k in ["Summary","Report","Feedback"]):
+            display_report(data)
+        elif "Summary" in data:
+            display_func("Summary",data.get("Summary"))
+
+        elif "Information" in data:
+            display_func("Information",data.get('Information'))
+
+    elif isinstance(data, list):
+        display_report(data)
+
 if st.button("NEXT"):
         next()
+ 
+if st.button("RESUBMIT"):
+    request(topic, task, True)
+    st.rerun()
+
 
 if st.session_state.error:
     st.error(st.session_state.get("error"))
